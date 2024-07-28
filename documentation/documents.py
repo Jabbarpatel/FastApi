be95b445554a64567a8fe8137567774937fde611
Jabbarpatel <115121477+Jabbarpatel@users.noreply.github.com>
2024-07-28 20:33:05 +0530

    Update documents.py

--- documentation/documents.py
+++ documentation/documents.py
@@ -1,9 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
 from typing import Union,Optional
 
 class AddNewUser(BaseModel):
     idx:int = None
-    first_name:str = None
-    last_name:str = None
+    first_name:str = Field(...,description='First name must be string')
+    last_name:str = Field(...,description='Last name must be string')
+    
+class UpdateUser(BaseModel):
+    first_name:str
+    last_name:str
     
     
