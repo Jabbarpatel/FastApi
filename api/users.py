0240efe3873fe670ad1a1226b48abf0b44d4c832
Jabbarpatel <115121477+Jabbarpatel@users.noreply.github.com>
2024-07-28 20:31:10 +0530

    Update users.py

--- api/users.py
+++ api/users.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from typing import Union
 from db.db import DB
-from documentation.documents import AddNewUser
+from documentation.documents import AddNewUser,UpdateUser
 
 user_route = APIRouter()
 
@@ -16,7 +16,6 @@ def get_users(page:int=None,page_size:int = None,filter:Union[str,int]=None):
        else:
            return str('User list is not found'),200
     except Exception as e:
-        print(e)
         return 'error',500
     
 @user_route.delete('/delete_user/{idx}')
@@ -27,19 +26,32 @@ def delete_user(idx:int = None):
             return 'success',200
         
     except Exception as e:
-        print(e)
         return str(e),500
     
 @user_route.post('/add_user')
 def add_user(request:AddNewUser):
     try:
+       
         idx = request.idx
         first_name = request.first_name
         last_name = request.last_name
-       
-        if idx and first_name and last_name:
-            pass
-        
+        DB.add_user(idx,first_name,last_name)
+        return 'success'
+    
+    except Exception as e:
+        return str(e),500
+    
+@user_route.post('/update_user/{idx}')
+def update_user(request:UpdateUser,idx:int = None):
+    try:
+        if idx:
+            first_name = request.first_name
+            last_name = request.last_name
+            if first_name and last_name:
+                DB.update_user(idx,first_name,last_name)
+                return 'success',200
+            else:
+                return str('parameters are missing in request body')
     except Exception as e:
         print(e)
-        return 'error',500
+        return str(e)
