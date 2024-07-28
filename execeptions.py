49bf14777e7233f0848dd16596cccfaa6d12f3af
Jabbarpatel <115121477+Jabbarpatel@users.noreply.github.com>
2024-07-28 20:30:37 +0530

    Update execeptions.py

--- execeptions.py
+++ execeptions.py
@@ -1,8 +1,12 @@
 
-class DeleteUserExeception(Exception):
+class UserNotFoundExeception(Exception):
     def __init__(self,idx):
         super().__init__(f'Record doesnot exists with idx {idx}')
         
 class UserAlreadyExistsExeception(Exception):
     def __init__(self,idx):
-        super().__init__(f'This user exists with {idx}')
+        super().__init__(f'This user already exists with id {idx}')
+
+class UserIdxListNotFound(Exception):
+    def __init__(self):
+        super().__init__(f'User idx list not found')
