63ef871373a25097cb2644843658fdaa566c4895
Jabbarpatel <115121477+Jabbarpatel@users.noreply.github.com>
2024-08-02 18:19:57 +0530

    Update models.py

--- models/models.py
+++ models/models.py
@@ -6,5 +6,5 @@ class Users(Base):
     idx = Column(INTEGER,primary_key=True,autoincrement=True)
     name = Column(String(100))
     sname = Column(String(100))
-    
+# this creates a table if the table is not exists in the database
 Base.meta_data.createall(CONNECTION)
